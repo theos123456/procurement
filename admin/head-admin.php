386a00c7347b9515b934ecfb01a4cd14ff6bf9e3
Happyfo <agnesu796@gmail.com>
2019-03-18 19:47:26 +0200

    first commit

---
+++ admin/head-admin.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Admin</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/font-awesome.css">
+    <link rel="stylesheet" href="../css/animate.min.css">
+  </head>
+  <body class="admin">
+
+  <div class="nav-admin">
+
+                <h3>Admin</h3>
+
+                  <a href="#"> <i class="fa fa-home"></i> Home</a>
+                  <a href="#"> <i class="fa fa-home"></i> Home</a>
+                  <a href="#"> <i class="fa fa-home"></i> Home</a>
+                  <a href="#"> <i class="fa fa-home"></i> Home</a>
+  </div>
+  <div class="cont-admin">
---
+++ admin/home_admin.php
@@ -0,0 +1,17 @@
+<?php include_once 'head-admin.php'; ?>
+
+<h1> HOME </h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include_once 'footer-admin.php'; ?>
